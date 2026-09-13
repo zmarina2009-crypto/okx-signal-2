e2b8679cc8fae5a5573cb4e49270a0524e8c00d3
zmarina2009-crypto <zmarina2009@gmail.com>
2026-09-13 12:36:18 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C="okx-signal-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
